39598641db51b6778a71b363ac918d65029f4a94
xavier <adelais.caulfield@outlook.com>
2023-03-13 11:59:36 +0100

    primer dia

---
+++ Primer_Proyecto.py
@@ -0,0 +1,9 @@
+print('Hola "mundo" que tal')
+print("comillaaaas 'aaa' diferentes, mismo resultado")
+print(100 + 50)
+print("Hola" + " " + 'Fede')
+print("Esta es una linea\nY esta es otra linea")
+print("Esto es una barra invertida \\")
+print("tu nombre es " + input("dime tu nombre: ")+ " " + input("dime tu apellido: "))
+
+print("tu nombre de cerveza es: \n " + input("dime el nombre de tu animal preferido: ")+ " " + input("dime tu color preferido: "))
