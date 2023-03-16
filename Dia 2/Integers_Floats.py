b50e7901899f3b564245c9702e8731722ecbd888
xavier <adelais.caulfield@outlook.com>
2023-03-16 11:41:29 +0100

    aprendiendo variables

---
+++ Dia 2/Integers_Floats.py
@@ -0,0 +1,17 @@
+mi_numero = 1.8
+print(mi_numero)
+
+print(type(mi_numero))
+
+mi_numero = mi_numero + mi_numero
+print(mi_numero)
+
+edad = input("dime tu edad: ")
+print(type(edad))
+
+edad = float(edad)
+
+print(type(edad))
+
+nuevo_numero = mi_numero + edad
+print(nuevo_numero)
---
+++ Dia 2/variables.py
@@ -0,0 +1,14 @@
+nombre = "Juan"
+print(nombre)
+
+nombre = "Laura"
+print(nombre)
+
+edad = 30
+edad2 = 15
+print(edad+edad2)
+
+nombre2 = " python"
+
+frase = nombre + nombre2
+print(frase)
