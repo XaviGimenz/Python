adeed416686e9809f9e00d816bb669876826b83f
xaviergimenez <adelais.caulfield@outlook.com>
2023-04-13 11:26:02 +0200

    Aprendiendo pathlib

--- Dia 6/Directorios.py
+++ Dia 6/Directorios.py
@@ -1,7 +1,18 @@
 import os
 
-ruta = os.getcwd()
+"""ruta = os.getcwd()
 print(ruta)
 
 ruta = os.chdir('')
-print(ruta)
+print(ruta)"""
+
+"""ruta = os.makedirs('/Users/xaviergimenez/Desktop/prueba')"""
+
+ruta = '/Users/xaviergimenez/Desktop/prueba/prueba.txt'
+
+elemento = os.path.split(ruta)
+print(elemento)
+
+"""os.rmdir('/Users/xaviergimenez/Desktop/prueba')"""
+
+otro_archivo = '/Users/xaviergimenez/Desktop/prueba'
---
+++ Dia 6/Pathlib.py
@@ -0,0 +1,15 @@
+from pathlib import Path, PureWindowsPath
+
+carpeta = Path('/Users/xaviergimenez/Desktop/prueba/prueba.txt')
+
+print(carpeta.read_text())
+print(carpeta.name)
+print(carpeta.suffix)
+print(carpeta.stem)
+if not carpeta.exists():
+    print("Este archivo no existe")
+else:
+    print("El archivo existe")
+
+ruta_windows = PureWindowsPath(carpeta)
+print(ruta_windows)
