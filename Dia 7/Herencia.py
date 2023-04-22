e587f94dc84b7b3706d3515ad1de0bb0c1b65a64
xaviergimenez <adelais.caulfield@outlook.com>
2023-04-22 11:18:02 +0200

    Empezando dia 7

---
+++ Dia 7/Herencia.py
@@ -0,0 +1,21 @@
+class Animal:
+
+    def __init__(self, edad, color):
+        self.edad = edad
+        self.color = color
+
+    def nacer(self):
+        print('Este animal ha nacido')
+
+class Pajaro(Animal):
+    pass
+
+
+
+piolin = Pajaro(2, 'Amarillo')
+
+
+
+
+#print(Pajaro.__bases__)
+#print(Animal.__subclasses__())
